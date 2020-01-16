f025a9cc6e89a388cabebdf530ef634b62cbadde
Offpics <kornelux1@gmail.com>
2020-01-16 20:36:08 +0100

    Add landing page

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import Image from 'react-bootstrap/Image';
+import { LinkContainer } from 'react-router-bootstrap';
+import Button from 'react-bootstrap/Button';
 
 class Home extends React.Component {
   render() {
-    return <h1>Home Page</h1>;
+    return (
+      <>
+        <Image
+          className="home--tank"
+          src={process.env.PUBLIC_URL + 'tank.svg'}
+          fluid
+          rounded
+        />
+        <h1>Tankify</h1>
+        <h2>Motto</h2>
+        <h3>Some catchy text here</h3>
+      </>
+    );
   }
 }
 
